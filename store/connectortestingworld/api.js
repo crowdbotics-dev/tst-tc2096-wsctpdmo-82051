b59c3e7de86f7a86112a2b09c1229a866aadb63a
Crowdbotics <engineering@crowdbotics.com>
2023-05-09 05:33:41 +0000

    Update store.

---
+++ store/connectortestingworld/api.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+import {
+  CONNECTORTESTINGWORLD_USERNAME,
+  CONNECTORTESTINGWORLD_PASSWORD
+} from "react-native-dotenv"
+const connectortestingworld = axios.create({
+  baseURL: "https://google.com",
+  auth: {
+    username: CONNECTORTESTINGWORLD_USERNAME,
+    password: CONNECTORTESTINGWORLD_PASSWORD
+  },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function connectortestingworld_get_testing_test_path_read(payload) {
+  return connectortestingworld.get(`/testing/${payload.test}/${payload.path}`)
+}
+export const apiService = { connectortestingworld_get_testing_test_path_read }
